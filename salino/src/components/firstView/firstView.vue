9beecad082502e3d92e9597f6217d8178cf17acc
pushmyandy <1805311490@qq.com>
2018-08-05 19:16:28 +0800

    no message

---
+++ salino/src/components/firstView/firstView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'firstView'
+}
+</script>
+
+<style scoped>
+
+</style>
--- salino/src/router/index.js
+++ salino/src/router/index.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Main from '../components/main/main'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  mode: 'history',
+  routes: [
+    {
+      name: 'main',
+      component: Main,
+      path: '/'
+    }
+  ]
 })
